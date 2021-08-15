3d686382a334906de8dc6fdce51b77f3d8221281
AgusKKhaer <aguskkhaer@gmail.com>
2021-08-15 19:55:05 +0700

    edit beberapa yang kurang, kemudian menambahkan widget di halaman profile dan messages

---
+++ lib/data_dummy/discount_data.dart
@@ -0,0 +1,36 @@
+
+
+class DiscountData {
+  static const List<Map<String, dynamic>> mapDiscount = [
+    {
+      "code_voucher":"GF5MCD",
+      "discount":"50%",
+      "partner_name":"GrabFood",
+      "ic_voucher" : "assets/icons/ic-grab-food-promo.png",
+    },
+    {
+      "code_voucher":"SATUS",
+      "discount":"25%",
+      "partner_name":"GrabFood",
+      "ic_voucher" : "assets/icons/ic-grab-food-promo.png",
+    },
+    {
+      "code_voucher":"BARENGSTARBUCKSOVO",
+      "discount":"55rb",
+      "partner_name":"GrabFood",
+      "ic_voucher" : "assets/icons/ic-grab-food-promo.png",
+    },
+    {
+      "code_voucher":"GMREKOMENDASIOVO",
+      "discount":"30%",
+      "partner_name":"GrabMart",
+      "ic_voucher" : "assets/icons/ic-grab-food-promo.png",
+    },
+    {
+      "code_voucher":"BERSATUDONASI",
+      "discount":"5rb",
+      "partner_name":"GrabTransport",
+      "ic_voucher" : "assets/icons/ic-grab-food-promo.png",
+    },
+  ];
+}
---
+++ lib/data_dummy/restaurant_data.dart
@@ -0,0 +1,39 @@
+class RestaurantData{
+  static const List<Map<String, dynamic>> mapRestaurant = [
+    {
+      "restaurant_name" : "Mister Burger",
+      "img_restaurant_url" : "https://misterburger.co.id//assets/uploads/c418c78ff9d369c3f8309f33c5d590e0.jpg?w=632&h=632",
+      "distance" :  2.2,
+      "rating" :  4.6,
+      "is_promo" :  true,
+    },
+    {
+      "restaurant_name" : "Mister Burger",
+      "img_restaurant_url" : "https://misterburger.co.id//assets/uploads/c418c78ff9d369c3f8309f33c5d590e0.jpg?w=632&h=632",
+      "distance" :  2.2,
+      "rating" :  4.6,
+      "is_promo" :  true,
+    },
+    {
+      "restaurant_name" : "Mister Burger",
+      "img_restaurant_url" : "https://misterburger.co.id//assets/uploads/c418c78ff9d369c3f8309f33c5d590e0.jpg?w=632&h=632",
+      "distance" :  2.2,
+      "rating" :  4.6,
+      "is_promo" :  true,
+    },
+    {
+      "restaurant_name" : "Mister Burger",
+      "img_restaurant_url" : "https://misterburger.co.id//assets/uploads/c418c78ff9d369c3f8309f33c5d590e0.jpg?w=632&h=632",
+      "distance" :  2.2,
+      "rating" :  4.6,
+      "is_promo" :  true,
+    },
+    {
+      "restaurant_name" : "Mister Burger",
+      "img_restaurant_url" : "https://misterburger.co.id//assets/uploads/c418c78ff9d369c3f8309f33c5d590e0.jpg?w=632&h=632",
+      "distance" :  2.2,
+      "rating" :  4.6,
+      "is_promo" :  true,
+    },
+  ];
+}
